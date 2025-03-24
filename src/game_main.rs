81d22892a213935a65f6bdee4a7517e21b874587
VeronikaBei <bey.verornika@gmail.com>
2025-03-24 18:24:35 +0100

    delta_second and camera

--- src/game_main.rs
+++ src/game_main.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
-use bevy::input::ButtonInput;
 use std::sync::{Arc, Mutex};
 use RustMP::server::Server;
 use RustMP::client::Client;
-
-
+use bevy::input::keyboard::KeyCode;
+use bevy::input::Input;
+use bevy::input::ButtonInput;
+use bevy::app::Startup;
+use bevy::app::Update;
 
 #[derive(Resource)]
 struct GameWindow {
@@ -13,11 +15,8 @@ struct GameWindow {
     title: String,
 }
 
-
 #[derive(Resource, Default)]
-struct GameState {
-}
-
+struct GameState;
 
 pub struct GameHandle {
     game_state: Arc<Mutex<GameState>>,
@@ -26,34 +25,27 @@ pub struct GameHandle {
 }
 
 impl GameHandle {
-    fn launch_server(&mut self) -> Result<(),std::io::Error>{
-        self.server = Some(Arc::new(Mutex::new(Server::new().unwrap())));
-
-        if let Some(server) = self.server.take() {
-            let server_clone = Arc::clone(&server);
-            server.lock().unwrap().start(server_clone);
-        }
+    fn launch_server(&mut self) -> Result<(), std::io::Error> {
+        let server = Arc::new(Mutex::new(Server::new().unwrap()));
+        server.lock().unwrap().start(Arc::clone(&server));
+        self.server = Some(server);
         Ok(())
     }
 
-
-    fn launch_client(&mut self,server_ip: String) -> Result<(), String> {
+    fn launch_client(&mut self, server_ip: String) -> Result<(), String> {
         if server_ip.is_empty() {
             return Err("No IP address provided".to_string());
         }
 
-        self.client = Some(Arc::new(Mutex::new(Client::new(server_ip).unwrap())));
-
-        if let Some(client) = self.client.take() {
-            let client_clone = Arc::clone(&client);
-            client.lock().unwrap().start(client_clone);
-        }
+        let client = Arc::new(Mutex::new(Client::new(server_ip).unwrap()));
+        client.lock().unwrap().start(Arc::clone(&client));
+        self.client = Some(client);
         Ok(())
     }
 
     fn construct_client(game_state: Arc<Mutex<GameState>>, server_ip: String) -> Self {
-        let mut handle:GameHandle = GameHandle {
-            game_state: game_state,
+        let mut handle = GameHandle {
+            game_state,
             client: None,
             server: None,
         };
@@ -62,17 +54,15 @@ impl GameHandle {
     }
 
     fn construct_server(game_state: Arc<Mutex<GameState>>) -> Self {
-        let mut handle:GameHandle = GameHandle {
-            game_state: game_state,
+        let mut handle = GameHandle {
+            game_state,
             client: None,
             server: None,
         };
-        handle.launch_server().expect("Failed to launch client");
+        handle.launch_server().expect("Failed to launch server");
         handle
     }
 }
-
-
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -82,22 +72,22 @@ fn main() {
     }
 
     let is_server = args[1].parse::<bool>().unwrap_or(false);
-    let ip_string = &args[2];
+    let ip_string = args[2].clone();
 
     let game_state = Arc::new(Mutex::new(GameState::default()));
-    let game_state_clone = game_state.clone();
-    
+
     let window = GameWindow {
         width: 800.0,
         height: 600.0,
         title: String::from("Bevy Game"),
     };
-    
-    if is_server{
-        GameHandle::construct_server(game_state_clone);
-    }else{
-        GameHandle::construct_client(game_state_clone, ip_string.clone());
+
+    if is_server {
+        GameHandle::construct_server(Arc::clone(&game_state));
+    } else {
+        GameHandle::construct_client(Arc::clone(&game_state), ip_string);
     }
+
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -109,12 +99,11 @@ fn main() {
         }))
         .insert_resource(window)
         .insert_resource(GameState::default())
+        .add_systems(Startup, setup_camera)
         .add_systems(Startup, spawn_player)
+        .add_systems(Update, move_player)
         .run();
-    
-    
 }
-
 
 #[derive(Component)]
 struct Player {
@@ -123,32 +112,44 @@ struct Player {
 
 fn spawn_player(mut commands: Commands) {
     commands.spawn((
-        Sprite {
-            color: Color::rgb(0.3, 0.5, 0.8),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.3, 0.5, 0.8),
+                custom_size: Some(Vec2::new(50.0, 50.0)),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..Default::default()
         },
-        Transform::from_xyz(0.0, 0.0, 0.0),
-        Player { speed: 5.0 },
+        Player { speed: 200.0 },
     ));
 }
 
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn move_player(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
+    keyboard_input: Res<Input<KeyCode>>,  
+    time: Res<Time>,                      
     mut query: Query<(&Player, &mut Transform)>,
 ) {
     for (player, mut transform) in query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
         if keyboard_input.pressed(KeyCode::KeyW) {
-            transform.translation.y += player.speed;
+            direction.y += 1.0;
         }
         if keyboard_input.pressed(KeyCode::KeyS) {
-            transform.translation.y -= player.speed;
+            direction.y -= 1.0;
         }
         if keyboard_input.pressed(KeyCode::KeyD) {
-            transform.translation.x += player.speed;
+            direction.x += 1.0;
         }
         if keyboard_input.pressed(KeyCode::KeyA) {
-            transform.translation.x -= player.speed;
+            direction.x -= 1.0;
         }
+
+        transform.translation += direction.normalize_or_zero() * player.speed * time.delta_seconds();
     }
 }
